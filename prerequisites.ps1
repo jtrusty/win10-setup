63d45368fc4181dae615c3eaef52e9f185a58a8d
Justin Trusty <justintrusty@gmail.com>
2019-06-09 22:31:48 -0500

    Update prerequisites.ps1

--- prerequisites.ps1
+++ prerequisites.ps1
@@ -9,25 +9,6 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     Exit
 }
 
-# Boxstarter options
-$Boxstarter.RebootOk=$true # Allow reboots?
-$Boxstarter.NoPassword=$false # Is this a machine with no login password?
-$Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
-
-# From a Administrator PowerShell, if Get-ExecutionPolicy returns Restricted, run:
-if ((Get-ExecutionPolicy) -eq "Restricted") {
-    Set-ExecutionPolicy Unrestricted -Force
-}
-
-#Set ExplorerOptions -showHidenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
-Enable-RemoteDesktop
-Disable-InternetExplorerESC
-Set-StartScreenOptions -EnableBootToDesktop
-
-# Update Windows and reboot if necessary
-Install-WindowsUpdate -AcceptEula
-if (Test-PendingReboot) { Invoke-Reboot }
-
 ### Chocolatey Installs ###
 write-host "Installing/Upgrading Chocolatey" -ForegroundColor "Yellow"
 
