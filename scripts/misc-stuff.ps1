f28567ae8e3f5be6002afbdbc9861782fbb9340f
Justin Trusty <justintrusty@gmail.com>
2019-06-17 21:20:25 -0500

    Update misc-stuff.ps1

--- scripts/misc-stuff.ps1
+++ scripts/misc-stuff.ps1
@@ -2,3 +2,9 @@
 cinst -y firacode
 cinst -y inconsolata
 cinst -y hackfont
+
+# Install Ubuntu for WSL
+# https://www.microsoft.com/en-us/p/ubuntu-1804-lts/9n9tngvndl3q
+
+# Install spaceship prompt
+npm install -g spaceship-prompt
