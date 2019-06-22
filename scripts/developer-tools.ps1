d192e926e10ffdfb56287b16ed47a1189299dca7
Justin Trusty <justintrusty@gmail.com>
2019-06-22 09:35:14 -0500

    Update developer-tools.ps1

--- scripts/developer-tools.ps1
+++ scripts/developer-tools.ps1
@@ -1,11 +1,16 @@
 # Common Development Tools
 choco upgrade git.install --params="/WindowsTerminal /NoShellIntegration /GitAndUnixToolsOnPath"
+choco upgrade docker-desktop
+choco upgrade postman
+
+# Programing Binaries
 choco upgrade nodejs
 choco upgrade python --version 2.7.2
 choco upgrade python
-choco upgrade docker-desktop
-choco upgrade postman
+
+# VSBuild Tools
 choco upgrade dotnetcore-sdk
+choco upgrade vcbuildtools
 
 # Development Tools - Visual Studio Code
 choco upgrade visualstudiocode
